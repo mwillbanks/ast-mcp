295254f22a90bdee6fb79ee795439eccaac2e90e
Mike Willbanks <me@mike.engineer>
2026-07-31 13:27:00 -0500

    chore(deps): update Fallow to 3.10.0

--- src/ast-bro/rewrite-capability.ts
+++
@@ -1,9 +0,0 @@
-import { astCapable } from "./capability";
-
-export async function astRewritable(
-  filePath: string,
-  language?: string,
-): Promise<boolean> {
-  if (language === "markdown") return false;
-  return astCapable(filePath, language);
-}
